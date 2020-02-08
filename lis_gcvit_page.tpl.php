04422b370bd0b6539acc026bd6a606d39f92402e
sdash <sdash@ncgr.org>
2020-02-08 10:00:43 -0600

    Added page title with genus.

--- lis_gcvit_page.tpl.php
+++ lis_gcvit_page.tpl.php
@@ -1,5 +1,6 @@
 <!-- Title and guide text  -->
 
+<h2>Genotype Comparison Visualization Tool (GCViT) for '<?php echo(ucfirst($third_path_component)); ?>' </h2>
 
 <!-- <iframe id="frameviewer" frameborder="0" width="1200" height="2500" scrolling="yes"  src="http://www.ncbi.nlm.nih.gov/pubmed?linkname=pubmed_pubmed_citedin&from_uid=15608283"  name="frameviewer"> -->
 <!--  ------------iframe gbrowse-----------------  -->
